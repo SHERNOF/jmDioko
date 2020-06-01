228db6dd7e567b286f6500926c359e0b53f823ad
shernof <sherwin.nofuente@yahoo.com>
2020-06-02 00:22:25 +1000

    1

--- js/app.js
+++ js/app.js
@@ -4,6 +4,39 @@ $(document).ready(function () {
 
   $(".scrollspy").scrollSpy()
 
+  // scroll fire
+
+  const options = [
+    {
+      selector: ".main-text",
+      offset: 0,
+      callback: function (el) {
+        Materialize.fadeInImage($(el))
+      }
+    },
+    {
+      selector: ".navbar-fixed",
+      offset: 1500,
+      callback: function () {
+        $("nav").removeClass("transparent")
+        $("nav").addClass("black darken-3")
+      }
+    }
+  ]
+  Materialize.scrollFire(options)
+
+  const option2 = [
+    {
+      selector: ".navbar-fixed",
+      offset: 1000,
+      callback: function () {
+        $("nav").removeClass("black darken-3")
+        $("nav").addClass("transparent")
+      }
+    }
+  ]
+  Materialize.scrollFire(option2)
+
   $(".count").each(function () {
     $(this)
       .prop("Counter", 0)
