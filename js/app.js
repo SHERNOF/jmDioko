30d3c249bed41b061206dca3bae659f8d8d8e93e
shernof <sherwin.nofuente@yahoo.com>
2020-06-02 19:09:06 +1000

    1

--- js/app.js
+++ js/app.js
@@ -25,6 +25,36 @@ $(document).ready(function () {
   ]
   // Materialize.scrollFire(options)
 
+  // const position = [-37.611911, 144.912327]
+  // const centerPosition = [-37.611911, 143.912327]
+
+  // const position = [42.361145, -71.057083]
+  // const centerPosition = [42.377815, -70.981133]
+
+  // function showGoogleMaps() {
+  //   const latLng = new google.maps.LatLng(position[0], position[1])
+  //   const centerLatLng = new google.maps.LatLng(centerPosition[0], centerPosition[1])
+
+  //   const mapOptions = {
+  //     zoom: 12,
+  //     streetViewControl: false,
+  //     scaleControl: true,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP,
+  //     center: centerLatLng
+  //   }
+  //   map = new google.maps.Map(document.getElementById("googlemap"), mapOptions)
+
+  //   // marker
+  //   marker = new google.maps.Marker({
+  //     position: latLng,
+  //     map: map,
+  //     draggable: false,
+  //     animation: google.maps.Animation.DROP
+  //   })
+  //   // map events
+  // }
+  // google.maps.event.addDomListener(window, "load", showGoogleMaps)
+
   $(".count").each(function () {
     $(this)
       .prop("Counter", 0)
